ddca8feb11f75afba053c60bcfa69a7dd509a883
Losiev2792 <lvlad2792@gmail.com>
2024-03-06 13:16:04 +0100

    hwFunction

--- learnArray.js
+++ learnArray.js
@@ -262,3 +262,25 @@ alert( i );}
             alert('Вы ввели число 2, а может и 3');
             break;
     }
+
+//Фильтрация по диапазону
+    function filterRange(arr,a,b){
+        return arr.filter(item => (a<= item && item<= b))
+    }
+    const arr =[5, 3, 8, 1];
+    const filtered = filterRange(arr , 1, 4);
+    alert( filtered );
+    alert( arr );
+
+//Сортировать в порядке по убыванию
+    let arry = [5, 2, 1, -10, 8]
+    arr.sort((a, b)=> b - a)
+    console.log(arry)
+
+//Трансформировать в массив имён
+    let vasya = { name: "Вася", age: 25 };
+    let petya = { name: "Петя", age: 30 };
+    let masha = { name: "Маша", age: 28 };
+    let users = [ vasya, petya, masha ];
+    let nameU =users.map(item=> item.name)
+    console.log(nameU);

b32ce98d21c70d828a3c015c419b9076902097c3
Losiev2792 <lvlad2792@gmail.com>
2024-03-11 22:34:45 +0100

    ObjectsTopic

--- learnArray.js
+++ learnArray.js
@@ -326,13 +326,6 @@ alert( i );}
 // Измените значение свойства name на Pete.
 // Удалите свойство name из объекта.
 
-        let user = {}
-
-        user.name = "John"
-        user.surname = "Smith"
-        user.name = "Pete";
-        delete user.name;
-
         //Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
 //
 // Должно работать так:
@@ -345,11 +338,6 @@ alert( i );}
 //
 // alert( isEmpty(schedule) ); // false
 
-        function isEmpty(obj){
-            for (let key in obj) {
-                return false;
-            }
-            return true;}
 
 
         //У нас есть объект, в котором хранятся зарплаты нашей команды:
@@ -363,16 +351,6 @@ alert( i );}
 //
 // Если объект salaries пуст, то результат должен быть 0.
 
-        let salaries = {
-            John: 100,
-            Ann: 160,
-            Pete: 130
-        }
-
-        let sum = 0;
-        for (let key in salaries) {
-            sum += salaries[key];
-        }
 
 
         //Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
@@ -406,3 +384,132 @@ menu = {
 
 
 
+//Напишите код, выполнив задание из каждого пункта отдельной строкой:
+// Создайте пустой объект user.
+// Добавьте свойство name со значением John.
+// Добавьте свойство surname со значением Smith.
+// Измените значение свойства name на Pete.
+// Удалите свойство name из объекта.
+
+
+let user = {
+    name: "John",
+    surname: "Smith",
+}
+        user.name = "Pete"
+            delete user.name;
+
+        //Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+//
+// Должно работать так:
+//
+// let schedule = {};
+//
+// alert( isEmpty(schedule) ); // true
+//
+// schedule["8:30"] = "get up";
+//
+// alert( isEmpty(schedule) ); // false
+
+        function isEmpty(obj) {
+            for (let key in obj) {
+                return false;
+            }
+            return true;
+        }
+
+        //У нас есть объект, в котором хранятся зарплаты нашей команды:
+// Напишите код для суммирования всех зарплат и сохраните результат в переменной sum. Должно получиться 390.
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+        const sum = 0;
+        for (let key in salaries) {
+            sum += salaries[key]
+        }
+
+        //Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+        //
+        // Например:
+        //
+        // // до вызова функции
+        let menu = {
+          width: 200,
+          height: 300,
+          title: "My menu"
+        };
+
+        multiplyNumeric(menu);
+
+        // // после вызова функции
+        menu = {
+          width: 400,
+          height: 600,
+          title: "My menu"
+        };
+        // Обратите внимание, что multiplyNumeric не нужно ничего возвращать. Следует напрямую изменять объект.
+
+
+        function multiplyNumeric(obj) {
+            for (let key in obj) {
+                if (typeof obj[key] == 'number') {
+                    obj[key] *= 2;
+                }}}
+
+        //Создайте объект calculator (калькулятор) с тремя методами:
+//
+// read() (читать) запрашивает два значения и сохраняет их как свойства объекта с именами a и b.
+// sum() (суммировать) возвращает сумму сохранённых значений.
+// mul() (умножить) перемножает сохранённые значения и возвращает результат.
+
+        let calculator = {
+            sum() {
+                return this.a + this.b;
+            },
+            mul() {
+                return this.a * this.b;
+            },
+            read() {
+                this.a = +prompt('a?', 0);
+                this.b = +prompt('b?', 0);
+            }
+        };
+        calculator.read();
+        alert( calculator.sum() );
+        alert( calculator.mul() );
+
+
+
+
+     //   Возможно ли создать функции A и B, чтобы new A() == new B()?
+
+        let obj = {};
+        function A() {return obj;}
+        function B() {return obj;}
+        alert( new A() == new B())
+
+        // Создайте функцию-конструктор Calculator, которая создаёт объекты с тремя методами:
+// read() запрашивает два значения при помощи prompt и сохраняет их значение в свойствах объекта.
+// sum() возвращает сумму этих свойств.
+// mul() возвращает произведение этих свойств.
+
+        function calculator() {
+
+            this.read = function() {
+                this.a = +prompt('a?', 0);
+                this.b = +prompt('b?', 0);
+            }
+            this.sum = function() {
+                return this.a + this.b;
+            }
+            this.mul = function() {
+                return this.a * this.b;
+            };
+        }
+        let calculator = new Calculator();
+        calculator.read();
+        alert( "Sum=" + calculator.sum());
+        alert( "Mul=" + calculator.mul());
